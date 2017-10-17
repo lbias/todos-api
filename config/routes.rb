5dcc4a3320895eb45754fad3a53b520e7c584133
lbias <306508354@qq.com>
2017-10-17 19:41:29 +0800

    move the existing todos and todo-items resources into a v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :todos do
-    resources :items
-  end  
+  # module the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
+  end
 
   post 'auth/login', to: 'authentication#authenticate'
   
