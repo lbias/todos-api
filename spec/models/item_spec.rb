c55fb10d9077fda230c0fe197bbf62a0d37b68fa
lbias <306508354@qq.com>
2017-10-17 13:45:00 +0800

    specs model item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
+# test suite for the model item
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # association test
+  # ensure an item record belongs to a single todo record
+  it {should belong_to(:todo)}
+  # validation test
+  # ensure column name is present before saving
+  it {should validate_presence_of(:name)}
 end
