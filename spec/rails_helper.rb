bced2c0aa9d6b47eff7e046e3c4482333e39d6f9
lbias <306508354@qq.com>
2017-10-17 14:48:25 +0800

    helper method RequestSpecHelper#json, and configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -36,6 +36,8 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
+  config.include RequestSpecHelper, type: :request
+  
   # configure test suite, add `FactoryGirl` methods
   config.include FactoryGirl::Syntax::Methods
 
