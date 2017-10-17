169f86b8c3110e35d46bba1e8c2dce5f5cc91fa8
lbias <306508354@qq.com>
2017-10-17 17:16:38 +0800

    define model user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # association test
+  # ensure model user has a 1:m relationship with the model todo
+  it {should have_many(:todo)}
+  # validation tests
+  # ensure name, email and password_digest are present before save
+  it {should validate_presence_of(:name)}
+  it {should validate_presence_of(:email)}
+  it {should validate_presence_of(:password_digest)}
 end
